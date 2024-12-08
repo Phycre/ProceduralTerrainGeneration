b085141a592a5866cdc959117603d1cec97b5894
Zachary Zenner <zacharyzenner@gmail.com>
2024-12-08 11:21:28 -0800

    height based lighting

--- shader/terrainShader.js
+++ shader/terrainShader.js
@@ -1,9 +1,14 @@
 export const terrainVertexShader = `
+varying vec3 vPosition;
 varying vec2 vUv;
+
 void main() {
-    vUv = uv;
+    vPosition = position; 
+    vUv = uv; 
+
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
+
 `;
 
 export const terrainFragmentShader = `
@@ -12,23 +17,24 @@ uniform sampler2D grassTex;
 uniform sampler2D rockTex;
 uniform sampler2D splatMap;
 
+varying vec3 vPosition;
 varying vec2 vUv;
 
 void main() {
-    vec4 splat = texture2D(splatMap, vUv);
-    float total = splat.r + splat.g + splat.b;
-    if (total < 0.001) {
-        total = 1.0;
-    }
+    vec4 splatColor = texture2D(splatMap, vUv);
 
     vec4 sandColor = texture2D(sandTex, vUv);
     vec4 grassColor = texture2D(grassTex, vUv);
     vec4 rockColor = texture2D(rockTex, vUv);
 
-    vec4 finalColor = (sandColor * splat.r +
-                       grassColor * splat.g +
-                       rockColor * splat.b) / total;
+    vec4 terrainColor = splatColor.r * sandColor +
+                        splatColor.g * grassColor +
+                        splatColor.b * rockColor;
 
-    gl_FragColor = finalColor;
+    float heightFactor = (vPosition.y + 10.0) / 20.0; // Normalize height
+    terrainColor.rgb *= heightFactor;
+
+    gl_FragColor = terrainColor;
 }
+
 `;
